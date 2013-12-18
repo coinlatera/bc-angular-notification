3a89f89aaf6cdeee02f4a4eafe98bb266e434b31
Matthis <matthis.perrin@gmail.com>
2013-12-18 05:19:39 +0100

    Update bower and test app

--- test-app/main.js
+++ test-app/main.js
@@ -15,7 +15,7 @@ angular.module('buttercoinAngularNotificationTestApp', ['bc.angular-notification
                  }])
                  .controller('MainCtrl', function ($scope, Notifications, NotificationsBuilder) {
 
-  var getNotification = function (title, type, displayMode, category) {
+  var getNotification = function (title, type, displayMode, category, duration) {
     return {
       id: Math.floor(Math.random() * 999999),
       title: title,
@@ -24,13 +24,14 @@ angular.module('buttercoinAngularNotificationTestApp', ['bc.angular-notification
       display: displayMode,
       category: category,
       indexInCategory: counter,
-      customClass: 'notif' + counter
+      customClass: 'notif' + counter,
+      duration: duration
     };
   };
 
   var counter = 0;
 
-  $scope.showRandomNotif = function (displayMode, category) {
+  $scope.showRandomNotif = function (displayMode, category, duration) {
     if (displayMode === 'sticky') {
       Notifications.show(getNotification('This is the sticky notification no ' + (++counter) + '. It won\'t go away until the user dismiss it' , 'urgent', 'sticky'));
     }
@@ -39,7 +40,7 @@ angular.module('buttercoinAngularNotificationTestApp', ['bc.angular-notification
       type = type == 0 ? 'success' : type == 1 ? 'pending' : 'error';
       var titleNo = Math.floor(Math.random() * 3) % 3;
       var title = titleNo == 0 ? "Hey there!" : type == 1 ? ('This is the ' + type + ' notification no ' + (++counter)) : ('This is the ' + type + ' notification no ' + (++counter) + '. It will automatically disappear after a certain amount of time.');
-      Notifications.show(getNotification(title, type, 'active'));
+      Notifications.show(getNotification(title, type, 'active', category, duration));
     }
     else if (displayMode === 'banana') {
       Notifications.show(getNotification('<img src="dancing-banana.gif"/><br/>Yup, you can even put gif in notifications!', 'info', 'active'));
