14004bca56a775589bc98c9d055c6ac4808cc829
Matthis <matthis.perrin@gmail.com>
2014-01-03 07:32:22 +0100

    Update test app with urgent notifications

--- test-app/main.js
+++ test-app/main.js
@@ -15,13 +15,13 @@ angular.module('buttercoinAngularNotificationTestApp', ['bc.angular-notification
                  }])
                  .controller('MainCtrl', function ($scope, Notifications, NotificationsBuilder, NotificationsUI) {
 
-  var getNotification = function (title, type, displayMode, category, duration, urgent) {
+  var getNotification = function (title, type, displayMode, duration, urgent) {
     return NotificationsBuilder.buildNotification(type, title, '', displayMode, urgent, false, {}, duration);
   };
 
   var counter = 0;
 
-  $scope.showRandomNotif = function (displayMode, category, duration) {
+  $scope.showRandomNotif = function (displayMode, duration, urgent) {
     if (displayMode === 'sticky') {
       Notifications.show(getNotification('This is the sticky notification no ' + (++counter) + '. It won\'t go away until the user dismiss it' , 'urgent', 'sticky'));
     }
@@ -30,7 +30,7 @@ angular.module('buttercoinAngularNotificationTestApp', ['bc.angular-notification
       type = type == 0 ? 'success' : type == 1 ? 'pending' : 'error';
       var titleNo = Math.floor(Math.random() * 3) % 3;
       var title = titleNo == 0 ? "Hey there!" : type == 1 ? ('This is the ' + type + ' notification no ' + (++counter)) : ('This is the ' + type + ' notification no ' + (++counter) + '. It will automatically disappear after a certain amount of time.');
-      Notifications.show(getNotification(title, type, 'active', category, duration));
+      Notifications.show(getNotification(title, type, 'active', duration, urgent));
     }
     else if (displayMode === 'banana') {
       Notifications.show(getNotification('<img src="dancing-banana.gif"/><br/>Yup, you can even put gif in notifications!', 'info', 'active'));
